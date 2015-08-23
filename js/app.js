b5b26e57b4d2185a3c40261b5a2aada02aca62b3
ZB <Zoeb2760@gmail.com>
2015-08-23 10:55:33 -0400

    Added Animation

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function() { 
+	$('.ryu').mouseenter(function() {
+	/*{
+    alert('mouse entered .ryu div');
+  }); */
+	    $('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	/* 1. $(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  }) 
+  .mouseleave(function() {
+    alert('mouse left');
+  });
+}); or 2 
+	$('.ryu').mouseleave(function() {
+    alert('mouse left .ryu div');
+  }); */
+	.mouseleave(function() {
+	$('.ryu-ready').hide();
+	$('.ryu-still').show();
+    })
+
+    .mousedown(function() {
+    	/* console.log('mousedown')
+
+    }) */
+    //play hadouken sound
+     playHadouken();
+     $('.ryu-ready').hide();
+     $('.ryu-throwing').show();	
+     $('.hadouken').show().animate(
+	  {'left': '1040px'},
+		  500,
+		  function() {
+		    $(this).hide();
+		    $(this).css('left', '540px');
+	  }
+	);
+     
+     //$('.hadouken').show();
+     //animate hadouken to the right of the screen
+    })
+     
+     .mouseup(function() {
+      /*	console.log('mouseup')
+     }) */
+     
+     $('.ryu-throwing').hide();
+     $('.ryu-ready').show();
+     $('.hadouken').hide();
+     })
+
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
